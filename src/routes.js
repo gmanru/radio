daa74a4c38251a0373883907f89563b5271f5812
Gman <mrgman11@yandex.ru>
2020-08-25 11:50:16 +0300

    make it better

--- src/routes.js
+++ src/routes.js
@@ -10,7 +10,7 @@ import Radio from './store/actions/Radio';
 
 const BaseRouter = () => (
   <div>
-    <Route exact path="/" component={ArticleList} />{" "}
+    <Route exact path="/" component={Radio} />{" "}
     <Route exact path="/radio" component={Radio} />{" "}
     <Route exact path="/video_str" component={VideoStream} />{" "}
     <Route exact path="/articles/:articleID/" component={ArticleDetail} />{" "}
